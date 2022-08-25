c1b1b910e0ced7328b48b8817ba05177409a9163
Om Bhatnagar <omishb2001@gmail.com>
2022-08-25 12:24:38 +0530

    Tweaked testimonial card.

--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -3,20 +3,21 @@ import "./testimonialCard.css";
 const TestimonialCard = ({ img, profImg, name, des }) => {
 	return (
 		<div className='flex gap-10'>
-			<div class='min-w-[400px] max-h-[150px] py-2 px-8 bg-white shadow-lg rounded-lg my-20'>
-				<div class='flex justify-center -mt-16'>
-					<div class='border-[4px] rounded-full border-indigo-700'>
+			<div className='min-w-[400px] bg-white shadow-lg rounded-lg my-20 relative'>
+				<div className='absolute left-1/2 top-10 flex justify-center -mt-16'>
+					<div className='border-[4px] rounded-full border-[#1e47ff]'>
 						<img
-							class='w-20 h-20 object-cover rounded-full border-[.3rem] '
+							className='w-10 h-10 object-cover rounded-full border-[.3rem] '
 							src={img}
+							alt='company'
 						/>
 					</div>
 				</div>
-				<div className=' relative flex gap-6'>
-					<img className='relative w-20' src={profImg} alt='' />
-					<div className='mt-2'>
-						<div>{name}</div>
-						<div className='text-blue-600 font-bold'>{des}</div>
+				<div className='flex'>
+					<img className='w-20 rotate-[4deg]' src={profImg} alt='person' />
+					<div className='mt-6 ml-2'>
+						<div className='font-bold text-md'>{name}</div>
+						<div className='text-[#1e47ff] font-semibold text-sm'>{des}</div>
 					</div>
 				</div>
 			</div>
