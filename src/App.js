02b8b31eff605339594a38dc3cc1206f4d8459fe
Om Bhatnagar <omishb2001@gmail.com>
2022-08-25 12:35:58 +0530

    Removed chakra provider.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Invest from "./pages/Invest";
-import { ChakraProvider } from "@chakra-ui/react";
 import Raise from "./pages/Raise";
 import { MantineProvider } from "@mantine/core";
 import StartupSignup from "./pages/StartupSignup";
@@ -10,19 +9,17 @@ import Signup from "./pages/Signup";
 
 function App() {
 	return (
-		<ChakraProvider>
-			<MantineProvider withGlobalStyles withNormalizeCSS>
-				<Router>
-					<Routes>
-						<Route path='/' element={<HomePage />}></Route>
-						<Route path='/invest' element={<Invest />}></Route>
-						<Route path='/raise' element={<Raise />}></Route>
-						<Route path='/signup' element={<Signup />}></Route>
-						<Route path='/startupsignup' element={<StartupSignup />}></Route>
-					</Routes>
-				</Router>
-			</MantineProvider>
-		</ChakraProvider>
+		<MantineProvider withGlobalStyles withNormalizeCSS>
+			<Router>
+				<Routes>
+					<Route path='/' element={<HomePage />}></Route>
+					<Route path='/invest' element={<Invest />}></Route>
+					<Route path='/raise' element={<Raise />}></Route>
+					<Route path='/signup' element={<Signup />}></Route>
+					<Route path='/startupsignup' element={<StartupSignup />}></Route>
+				</Routes>
+			</Router>
+		</MantineProvider>
 	);
 }
 
